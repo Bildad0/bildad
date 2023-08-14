28e37a2aa6d7a436c3a69fd3046cee58f7b7b295
Bildad0 <bildadowuor@gmail.com>
2023-08-13 23:22:32 -0700

    updates

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 $(document).ready(function() {
 
 
-    // function scrollUp (){
-    //  this.scrollY>500? $("nav").style()
-
-    // }
-
-    //all jquery here
-
     jQuery('.tabs .tab-links a').on('click', function(e) {
         var currentAttrValue = jQuery(this).attr('href');
-
-        // Show/Hide Tabs
-        jQuery('.tabs ' + currentAttrValue).fadeIn(400).siblings().hide();
-
-        // Change/remove current tab to active
-        jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
-
+        jQuery('.tabs ' + currentAttrValue).fadeIn(400).siblings().hide(); // Show/Hide Tabs
+        jQuery(this).parent('li').addClass('active').siblings().removeClass('active'); // Change/remove current tab to active
         e.preventDefault();
     });
 
